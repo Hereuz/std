4db10b69fdb6a7eeb95ee12c6ce85e791c819222
Hereuz <mallet.valentin21@gmail.com>
2026-07-05 18:25:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,22 @@ const page1 = document.getElementById("page1");
 const page2 = document.getElementById("page2");
 const envelope = document.getElementById("envelope");
 
-// PAGE 1 -> PAGE 2
+// état initial : ENVELOPPE OUVERTE
+envelope.classList.add("open");
+
 envelope.addEventListener("click", () => {
-  envelope.classList.add("open");
+
+  // inversion animation : fermeture
+  envelope.classList.remove("open");
+  envelope.classList.add("close");
 
   setTimeout(() => {
     page1.classList.remove("active");
     page2.classList.add("active");
-  }, 600);
+
+    // reset pour prochain chargement éventuel
+    envelope.classList.remove("close");
+    envelope.classList.add("open");
+
+  }, 700);
 });
