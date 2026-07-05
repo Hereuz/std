0ceaf17178576e13c7672eb6cea76a1ef12e661c
Hereuz <mallet.valentin21@gmail.com>
2026-07-05 18:17:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+const page1 = document.getElementById("page1");
+const page2 = document.getElementById("page2");
+const envelope = document.getElementById("envelope");
 
+// PAGE 1 -> PAGE 2
+envelope.addEventListener("click", () => {
+  envelope.classList.add("open");
+
+  setTimeout(() => {
+    page1.classList.remove("active");
+    page2.classList.add("active");
+  }, 600);
+});
